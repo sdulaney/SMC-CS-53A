d2ad140d21ab0393fa8f8193669119e0ff4b4789
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-09-16 17:03:32 -0700

    add project 1 files

---
+++ Project1/Assignments.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+/*******************
+ CS53A
+Project1 (100 points)
+Please read on Swift String from apple document and try the following using Playground
+Or iOS as we discuss in class (if Playground running for ever, save your work and quit Xcode
+And start again).
+Let have String of emojies and apply the following methods.
+ 
+ let emojies ="⚽️🏀🏈⚾️🎱🏉🏐🎾🚖⏰🍎🍎";
+
+ Try to do the following:
+ 
+ 1. print all emojies
+ 2. count how may apples by goint thru emojies
+ 3. print all emojies up to 🎱
+ 4. print all emojies after 🎱
+ 5. add new emojies to the end
+ 6. remove the first emojie
+ 7. print emojies in reverse orther.
+ 
+ Make sure you document and show output after
+ each step.
+ 
+ 
+ 
+ **************************************/
