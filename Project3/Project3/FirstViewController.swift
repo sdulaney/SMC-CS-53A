ac4aa6008edea8c37d7dd68be81582a701a83081
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-11-13 19:03:45 -0800

    add project 3 files

---
+++ Project3/Project3/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Project3
+//
+//  Created by Stewart Dulaney on 11/13/18.
+//  Copyright © 2018 Stewart Dulaney. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Project3/Project3/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Project3
+//
+//  Created by Stewart Dulaney on 11/13/18.
+//  Copyright © 2018 Stewart Dulaney. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
