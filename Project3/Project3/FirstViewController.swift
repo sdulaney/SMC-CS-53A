b7c290890c3bf785ebb1e4ecde790a39115a1ebe
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-11-16 22:53:21 -0800

    add ShowClockInSwift code to FirstViewController

--- Project3/Project3/FirstViewController.swift
+++ Project3/Project3/FirstViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var clockLabel: UILabel!
+    
+    var timer = Timer()
+    
+    @objc func doTheTime() {
+        let currTime = NSDate()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss"
+        let currTimeFormatted = dateFormatter.string(from: currTime as Date)
+        clockLabel.text = currTimeFormatted
+    }
+    
+    @IBAction func startClockButton(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(FirstViewController.doTheTime), userInfo: nil, repeats: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
 
 }
 
