82d47ba838ca5d0c44c7adc775665ee9eb7a7a8d
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-12-11 19:54:46 -0800

    add ViewController logic

--- FinalExam/Dulaney_Stewart/ShowProtocol/ShowProtocol/ViewController.swift
+++ FinalExam/Dulaney_Stewart/ShowProtocol/ShowProtocol/ViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var arrayBuilding = [Appraisable]()
+    let b1 = Building(numFloors: 3, street: "10919 Wellworth Ave", city: "Los Angeles", state: "CA", zip: "90024")
+    let h1 = House(numFloors: 3, street: "655 Gayley Ave", city: "Los Angeles", state: "CA", zip: "90024", numBedrooms: 37, numBathrooms: 3)
+    let s1 = Store(numFloors: 1, street: "3212 Pico Blvd", city: "Santa Monica", state: "CA", zip: "90405", name: "Trader Joe's", storeManager: "Joe")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        arrayBuilding = [b1, h1, s1]
+        for appraisable in arrayBuilding {
+            if let building = appraisable as? Building {
+                print(building.description(), terminator:"")
+            }
+            printCostEstimate(a: appraisable)
+            print()
+        }
     }
 
 
 }
 
 func printCostEstimate(a: Appraisable) {
-    print("Cost Estimate: \(a.estimate())\n")
+    print("Cost Estimate: \(a.estimate())")
 }
