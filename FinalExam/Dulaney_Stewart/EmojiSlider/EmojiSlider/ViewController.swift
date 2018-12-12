d0a4b90800b59c3e37c61b9175d413561b949884
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-12-11 19:58:16 -0800

    add project files

---
+++ FinalExam/Dulaney_Stewart/EmojiSlider/EmojiSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiSlider
+//
+//  Created by Stewart Dulaney on 12/11/18.
+//  Copyright © 2018 Stewart Dulaney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
